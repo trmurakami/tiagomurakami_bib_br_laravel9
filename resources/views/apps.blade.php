18a23acd97a9c918f081329a057b026218f021bc
Tiago Murakami <trmurakami@gmail.com>
2022-12-19 09:13:02 -0300

    Link para apps online

--- resources/views/apps.blade.php
+++ resources/views/apps.blade.php
@@ -4,4 +4,25 @@
 
 @section('content')
 
+<div class="row mt-3">
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Online MARC Editor</h5>
+                <p class="card-text">Editor MARC.</p>
+                <a href="onlinemarceditor" class="btn btn-primary">Acessar</a>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Editor ALEPHSEQ para a Produção Científica USP</h5>
+                <p class="card-text">Editor.</p>
+                <a href="editoralephseq" class="btn btn-primary">Acessar</a>
+            </div>
+        </div>
+    </div>
+</div>
+
 @stop
