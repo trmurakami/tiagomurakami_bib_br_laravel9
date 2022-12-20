4e5d269325408228e4d10792f0e8b6bfec27638c
Tiago Murakami <trmurakami@gmail.com>
2022-12-20 11:31:55 -0300

    Things

--- app/Http/Requests/StoreThingRequest.php
+++ app/Http/Requests/StoreThingRequest.php
@@ -13,7 +13,7 @@ class StoreThingRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/UpdateThingRequest.php
+++ app/Http/Requests/UpdateThingRequest.php
@@ -13,7 +13,7 @@ class UpdateThingRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
