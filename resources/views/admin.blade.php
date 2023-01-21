af9241fa2820ced9c8814ee6c9561c24a3467fb4
Tiago Murakami <trmurakami@gmail.com>
2023-01-20 23:04:03 -0300

    Guia de softwares

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,40 +4,24 @@
 
 @section('content')
 
-<div class="row mt-3">
-    <div class="col-sm-6">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Softwares</h5>
-                <p class="card-text">Lista de softwares.</p>
-                <a href="softwares/create" class="btn btn-primary">Cadastrar</a>
+<div class="container">
+    <div class="row mt-3">
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Softwares</h5>
+                    <p class="card-text">Lista de softwares.</p>
+                    <a href="softwares" class="btn btn-primary">Cadastrar</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-sm-6">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Instituições</h5>
-                <p class="card-text">Lista de Instituições.</p>
-                <a href="things/create" class="btn btn-primary">Cadastrar</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-6 mt-3">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Fornecedores</h5>
-                <p class="card-text">Lista de fornecedores.</p>
-                <a href="#" class="btn btn-primary">Cadastrar</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-sm-6 mt-3">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Instituições</h5>
-                <p class="card-text">Lista de Instituições.</p>
-                <a href="#" class="btn btn-primary">Cadastrar</a>
+        <div class="col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Instituições</h5>
+                    <p class="card-text">Lista de Instituições.</p>
+                    <a href="things" class="btn btn-primary">Cadastrar</a>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/guiadesoftwares.blade.php
+++ resources/views/guiadesoftwares.blade.php
@@ -8,26 +8,34 @@
 
     <h1>Guia de softwares</h1>
 
-    Quantidade de softwares cadastrados: {{ count($softwares) }}
+    <p>O Guia de softwares para Bibliotecas e Unidades de Informação é banco de dados com o registro dos principais
+        softwares utilizado por
+        Instituições brasileiras para a gestão de recursos informacionais.</p>
 
-    <div class="container">
-        <div class="row mt-3">
-            @foreach ($softwares as $software)
-            <div class="col-sm-6 mt-3">
-                <div class="card">
-                    <img src="" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $software->name }}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $software->applicationcategory }}</h6>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $software->maintainer }}</h6>
-                        <h6 class="card-subtitle mb-2 text-muted">{{ $software->license }}</h6>
-                        <p class="card-text">{{ $software->description }}</p>
-                        <a href="software/{{ $software->id }}" class="btn btn-primary">Saiba mais</a>
-                    </div>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">{{ count($softwares) }}</h5>
+            <p class="card-text">Softwares registrados</p>
+        </div>
+    </div>
+
+
+    <div class="row mt-3">
+        @foreach ($softwares as $software)
+        <div class="col-sm-6 mt-3">
+            <div class="card">
+                <img src="" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $software->name }}</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ $software->applicationcategory }}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ $software->maintainer }}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">{{ $software->license }}</h6>
+                    <p class="card-text">{{ $software->description }}</p>
+                    <a href="software/{{ $software->id }}" class="btn btn-primary">Saiba mais</a>
                 </div>
             </div>
-            @endforeach
         </div>
+        @endforeach
     </div>
 
 
