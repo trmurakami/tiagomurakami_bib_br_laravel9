bbc8b8315878528e83d9ffebfadf0e842e4bbf21
Tiago Murakami <trmurakami@gmail.com>
2023-02-01 08:44:23 -0300

    Slides schema.org

--- resources/views/slides.blade.php
+++ resources/views/slides.blade.php
@@ -21,6 +21,19 @@
                 </div>
             </div>
         </div>
+        <div class="col-sm-6">
+            <div class="card">
+                <img src="includes/images/PPT_schema_org.png" class="card-img-top"
+                    alt="Slides da apresentação sobre o Schema.org">
+                <div class="card-body">
+                    <h5 class="card-title">Uma visão sobre o schema.org para as bibliotecas</h5>
+                    <p class="card-text">Slides apresentados no Grupo de Catalogação.
+                        <br />Ano: 2021
+                    </p>
+                    <a href="files/UmaVisaoSobreOSchema_org.pptx" class="btn btn-primary">Acessar</a>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
