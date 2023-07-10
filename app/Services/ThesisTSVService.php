c698ec999909076da34126030b8a774fa0d72edf
Tiago Murakami <trmurakami@gmail.com>
2023-07-10 20:42:28 -0300

    Correção

--- app/Services/ThesisTSVService.php
+++ app/Services/ThesisTSVService.php
@@ -27,55 +27,57 @@ class ThesisTSVService
                     foreach ($data as $key => $value) {
                         $label = data_get(HEADER, $key);
                         if ($label == 'NM_SUBTIPO_PRODUCAO') {
-                            $work->type = $value;
+                            $work->type = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                         }
                         if ($label == 'NM_PRODUCAO') {
-                            $work->name = $value;
+                            $work->name = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                         }
                         if ($label == 'AN_BASE') {
-                            $work->datePublished = $value;
+                            $work->datePublished = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                         }
                         if ($label == 'NM_IDIOMA') {
-                            $work->inLanguage = $value;
+                            $work->inLanguage = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                         }
                         if ($label == 'DS_URL_TEXTO_COMPLETO') {
-                            $work->url = $value;
+                            $work->url = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                         }
                         if ($label == 'DS_RESUMO') {
-                            $work->abstract = $value;
+                            $work->abstract = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                         }
                         if ($label == 'NM_PROGRAMA') {
-                            $work->inSupportOf = $value;
+                            $work->inSupportOf = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                         }
                         if ($label == 'NM_DISCENTE') {
                             $author_array = [];
                             $author_array[0]['id'] = '';
                             $author_array[0]['type'] = 'Person';
-                            $author_array[0]['name'] = $value;
+                            $author_array[0]['name'] = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                             $author_array[0]['function'] = 'Autor';
                         }
                         if ($label == 'NM_ORIENTADOR') {
                             $author_array[1]['id'] = '';
                             $author_array[1]['type'] = 'Person';
-                            $author_array[1]['name'] = $value;
+                            $author_array[1]['name'] = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                             $author_array[1]['function'] = 'Orientador';
                             $work->author = $author_array;
                             unset($author_array);
                         }
                         if ($label == 'NM_ENTIDADE_ENSINO') {
-                            $work->sourceOrganization = $value;
+                            $work->sourceOrganization = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                         }
                         if ($label == 'NR_PAGINAS') {
-                            $work->numberOfPages = $value;
+                            $work->numberOfPages = mb_convert_encoding($value, 'UTF-8', 'UTF-8');
                         }
                         if ($label == 'DS_PALAVRA_CHAVE') {
-                            $keywords = explode(';', $value);
+                            $keywords = explode(';', mb_convert_encoding($value, 'UTF-8', 'UTF-8'));
                             foreach ($keywords as $key => $value) {
                                 $keywords_array[$key]['id'] = "";
-                                $keywords_array[$key]['name'] = substr($value, 0, 255);
+                                $keywords_array[$key]['name'] = substr(mb_convert_encoding($value, 'UTF-8', 'UTF-8'), 0, 255);
                             }
-                            $work->about = $keywords_array;
-                            unset($keywords_array);
+                            if (is_array(json_decode(json_encode($keywords_array, true)))){
+                                $work->about = $keywords_array;
+                                unset($keywords_array);
+                            }
                         }
                     }
                     $saved_id = $work->save();
