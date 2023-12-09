b32b784174ea91a635083bd7c9f6665f582adb04
Tiago Murakami <trmurakami@gmail.com>
2023-12-08 22:21:17 -0300

    Ficha

--- resources/views/fichaPDF.blade.php
+++ resources/views/fichaPDF.blade.php
@@ -10,7 +10,7 @@
         margin: auto;
         border: 1px solid #000;
         padding: 20px;
-        font-size: 14px;
+        font-size: 12px;
         font-family: monospace;
         line-height: 1.5;
     }
@@ -21,6 +21,7 @@
 
     .ficha-catalografica p {
         text-indent: 50px;
+        margin: 0;
     }
     </style>
 </head>
@@ -59,16 +60,19 @@
     <p style="text-align: center;">CIP - Catalogação na Publicação</p>
     <div class="ficha-catalografica">
         <p>{{ $sobrenome }}, {{ $nome }}</p>
-        <p><strong>{{ $titulo }}</strong> / {{ $nome }} {{ $sobrenome }}. — {{ $ano }}.
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;<strong>{{ $titulo }}</strong> / {{ $nome }} {{ $sobrenome }}. — {{ $ano }}.
         </p>
-        <p>{{ $folhas }} f.
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;{{ $folhas }} f.
             @if ($ilustrado == ': il.')
             : il.
             @endif
         </p>
-        <p>{{ $genero_orientador }}: {{ $nome_orientador }} {{ $sobrenome_orientador }}.</p>
-        <p>Tese (doutorado) — {{ $instituicao }}, PPG, {{ $ano }}.</p>
-        <p>1. Assunto. 2. Assunto. I. {{ $sobrenome_orientador }}, {{ $nome_orientador }}. II. {{ $titulo }}.</p>
+        <br />
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;{{ $genero_orientador }}: {{ $nome_orientador }} {{ $sobrenome_orientador }}.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;Tese (doutorado) — {{ $instituicao }}, PPG, {{ $ano }}.</p>
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;1. Assunto. 2. Assunto. I. {{ $sobrenome_orientador }}, {{ $nome_orientador }}. II.
+            {{ $titulo }}.
+        </p>
     </div>
     <p style="text-align: center;">
         Elaborado pelo Gerador de ficha catalográfica para trabalhos acadêmicos com os dados fornecidos pelo(a) autor(a)
